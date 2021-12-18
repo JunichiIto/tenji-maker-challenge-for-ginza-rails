8fdf661cfe48a8c99554e4b36b20f711d0a7b1c4
fukurose <xylitoall@gmail.com>
2021-12-19 00:43:06 +0900

    feat: 文字を点字に変換する処理を追加

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TenjiMaker
   def to_tenji(text)
     # 以下はサンプルの仮実装なので、このcase文は全部消して自作ロジックに書き直すこと
@@ -39,5 +41,20 @@ class TenjiMaker
         -o o- o-
       TENJI
     end
+  end
+
+  def convert(char)
+    # 子音はnilの場合もあるので、reverse して母音から取得する
+    vowel, consonant = char.split(//).reverse
+
+    # vowel が「n」の場合は「ん」を返す
+    return TenjiBuilder::Consonant.new.nn if vowel.downcase == 'n'
+
+    # 母音の点字を作成
+    tenji_vowel = TenjiBuilder::Vowel.new.__send__(vowel.downcase)
+    return tenji_vowel unless consonant
+
+    # 子音+母音の点字を作成
+    TenjiBuilder::Consonant.new.__send__(consonant.downcase, tenji_vowel)
   end
 end
