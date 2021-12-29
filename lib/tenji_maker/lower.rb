5feda4b399520cbece490cb9c1c741c95611656a
Kazuki Kondo <k.kazuki526@gmail.com>
2021-12-29 17:08:41 +0900

    refactor: 母音だけであることが明確になるように、別メソッドにして名前をつける

--- lib/tenji_maker/lower.rb
+++ lib/tenji_maker/lower.rb
@@ -7,7 +7,7 @@ class TenjiMaker
     end
 
     def none?(char)
-      char.start_with?("R") || char.length == 1
+      char.start_with?("R") || only_vowel?(char)
     end
 
     def right?(char)
@@ -15,7 +15,12 @@ class TenjiMaker
     end
 
     def left?(char)
+      # 「YU」は[:all]である為、除外する
       char.start_with?("T", "N", "W", "Y") && char != "YU"
+    end
+
+    def only_vowel?(char)
+      char.length == 1
     end
   end
 end
