c5275d33c0807156712d87b5453bff3f2073a8b0
Kazuki Kondo <k.kazuki526@gmail.com>
2021-12-29 15:51:53 +0900

    feature: 上・中・下段の三階層に分けてから点字を組み立てる

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,43 +1,72 @@
 class TenjiMaker
+  TENJI_POINTS = { all: "oo", left: "o-", right: "-o", none: "--"}.freeze
+  private_constant :TENJI_POINTS
+
   def to_tenji(text)
-    # 以下はサンプルの仮実装なので、このcase文は全部消して自作ロジックに書き直すこと
-    case text
-    when 'A HI RU'
-      <<~TENJI.chomp
-        o- o- oo
-        -- o- -o
-        -- oo --
-      TENJI
-    when 'KI RI N'
-      <<~TENJI.chomp
-        o- o- --
-        o- oo -o
-        -o -- oo
-      TENJI
-    when 'SI MA U MA'
-      <<~TENJI.chomp
-        o- o- oo o-
-        oo -o -- -o
-        -o oo -- oo
-      TENJI
-    when 'NI WA TO RI'
-      <<~TENJI.chomp
-        o- -- -o o-
-        o- -- oo oo
-        o- o- o- --
-      TENJI
-    when 'HI YO KO'
-      <<~TENJI.chomp
-        o- -o -o
-        o- -o o-
-        oo o- -o
-      TENJI
-    when 'KI TU NE'
-      <<~TENJI.chomp
-        o- oo oo
-        o- -o o-
-        -o o- o-
-      TENJI
+    upper_points_position  = upper(text)
+    middle_points_position = middle(text)
+    lower_points_position  = lower(text)
+
+    [
+      tenji_format(upper_points_position),
+      tenji_format(middle_points_position),
+      tenji_format(lower_points_position),
+    ].join("\n")
+  end
+
+  private
+
+  def upper(text)
+    text.split(" ").map do |char|
+      next :none if char == "N"
+
+      if char.start_with?("W")
+        :none
+      elsif char.start_with?("Y") || char.end_with?("O")
+        :right
+      elsif char.end_with?("A", "I")
+        :left
+      elsif char.end_with?("U", "E")
+        :all
+      end
     end
+  end
+
+  def middle(text)
+    text.split(" ").map do |char|
+      next :right if char == "N"
+
+      if %w[A U KA KU NA NU HA HU YA YU WA].include?(char)
+        :none
+      elsif %w[SA SU TA TU MA MU RA RU YO WO].include?(char)
+        :right
+      elsif %w[I E O KI KE KO NI NE NO HI HE HO].include?(char)
+        :left
+      elsif %w[SI SE SO TI TE TO MI ME MO RI RE RO].include?(char)
+        :all
+      end
+    end
+  end
+
+  def lower(text)
+    text.split(" ").map do |char|
+      next :all if char == "N"
+
+      if char.start_with?("R") || char.length == 1
+        :none
+      elsif char.start_with?("K", "S")
+        :right
+      elsif char.start_with?("T", "N", "W", "Y") && char != "YU"
+        :left
+      else
+        :all
+      end
+    end
+  end
+
+  def tenji_format(points_position)
+    points_position.map do |position|
+      TENJI_POINTS[position]
+    end.join(" ")
   end
 end
