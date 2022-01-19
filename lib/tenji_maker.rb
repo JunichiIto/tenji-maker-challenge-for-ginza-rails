0c8a06836c297848f643b2b026c590ae52a43b2e
aim2bpg <honotaka2016@gmail.com>
2022-01-20 01:23:26 +0900

    Modify tenji maker part 1

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -1,43 +1,57 @@
+# frozen_string_literal: true
+
+TENJI_3BIT_TABLE = {
+  '0' => '---',
+  '1' => '--o',
+  '2' => '-o-',
+  '3' => '-oo',
+  '4' => 'o--',
+  '5' => 'o-o',
+  '6' => 'oo-',
+  '7' => 'ooo'
+}.freeze
+
+# 母音と子音を3bit単位で判別するために、点の有無を「① ④ ② ⑤ ③ ⑥ 」の順に列指向で並べている
 class TenjiMaker
   def to_tenji(text)
-    # 以下はサンプルの仮実装なので、このcase文は全部消して自作ロジックに書き直すこと
-    case text
-    when 'A HI RU'
-      <<~TENJI.chomp
-        o- o- oo
-        -- o- -o
-        -- oo --
-      TENJI
-    when 'KI RI N'
-      <<~TENJI.chomp
-        o- o- --
-        o- oo -o
-        -o -- oo
-      TENJI
-    when 'SI MA U MA'
-      <<~TENJI.chomp
-        o- o- oo o-
-        oo -o -- -o
-        -o oo -- oo
-      TENJI
-    when 'NI WA TO RI'
-      <<~TENJI.chomp
-        o- -- -o o-
-        o- -- oo oo
-        o- o- o- --
-      TENJI
-    when 'HI YO KO'
-      <<~TENJI.chomp
-        o- -o -o
-        o- -o o-
-        oo o- -o
-      TENJI
-    when 'KI TU NE'
-      <<~TENJI.chomp
-        o- oo oo
-        o- -o o-
-        -o o- o-
-      TENJI
+    tenji_serials = text.split(' ').map { |romaji| make_tenji(romaji) }
+    [0..1, 2..3, 4..5].map do |range|
+      tenji_serials.flatten.map { |tenji_serial| tenji_serial.slice(range) }.join(' ')
+    end.join("\n")
+  end
+
+  def make_tenji(romaji)
+    tenji_sokuon = romaji =~ /(.)\1/ ? ['--o---'] : []
+    romaji.squeeze!('KSTNHMYRWGZDBP')
+
+    tenji_boin = make_tenji_3bit('xxxOAIUE', romaji.slice(-1))
+    shiin = romaji.slice(0)
+
+    case romaji.length
+    when 1
+      "#{make_tenji_3bit('N-xOAIUE', romaji)}#{romaji =~/[AIUEO]/ ? '---' : make_tenji_3bit('xxxx-xxN', romaji)}"
+    when 2
+      case romaji
+      when /[YW]+/
+        ["#{make_tenji_3bit('WxYxxxxx', shiin)}#{make_tenji_3bit('xxAUxxOx', romaji.slice(-1))}"]
+      when /[GZDB]+/
+        ['---o--', "#{tenji_boin}#{make_tenji_3bit('xGxBxZDx', shiin)}"]
+      when /P+/
+        ['-----o', "#{tenji_boin}-oo"]
+      else
+        ["#{tenji_boin}#{make_tenji_3bit('xKNHRSTM', shiin)}"]
+      end.unshift(tenji_sokuon)
+    when 3
+      case romaji
+      when /[GZDB]Y+/
+        ['-o-o--', "#{tenji_boin}#{make_tenji_3bit('xGxBxZDx', shiin)}"]
+      when /PY+/
+        ['-o---o', "#{tenji_boin}-oo"]
+      else
+        ['-o----', "#{tenji_boin}#{make_tenji_3bit('xKNHRSTM', shiin)}"]
+      end.unshift(tenji_sokuon)
     end
   end
+
+  def make_tenji_3bit(target, char) = (target =~ /#{char}/).to_s.sub(/./, TENJI_3BIT_TABLE)
 end
--- test/tenji_maker_test.rb
+++ test/tenji_maker_test.rb
@@ -63,4 +63,184 @@ class TenjiMakerTest < Minitest::Test
   # ここから上のテストは変更不可 =====================
 
   # 独自のテストパターンを追加するのは自由です
+
+  # チーター
+  def test_tii_taa
+    tenji = @tenji_maker.to_tenji('TI - TA -')
+    assert_equal <<~TENJI.chomp, tenji
+      o- -- o- --
+      oo oo -o oo
+      o- -- o- --
+    TENJI
+  end
+
+  # ゴリラ
+  def test_go_ri_ra
+    tenji = @tenji_maker.to_tenji('GO RI RA')
+    assert_equal <<~TENJI.chomp, tenji
+      -- -o o- o-
+      -o o- oo -o
+      -- -o -- --
+    TENJI
+  end
+
+  # ネズミ
+  def test_ne_zu_mi
+    tenji = @tenji_maker.to_tenji('NE ZU MI')
+    assert_equal <<~TENJI.chomp, tenji
+      oo -- oo o-
+      o- -o -o oo
+      o- -- -o oo
+    TENJI
+  end
+
+  # カバ
+  def test_ka_ba
+    tenji = @tenji_maker.to_tenji('KA BA')
+    assert_equal <<~TENJI.chomp, tenji
+      o- -- o-
+      -- -o --
+      -o -- oo
+    TENJI
+  end
+
+  # パンダ
+  def test_pa_n_da
+    tenji = @tenji_maker.to_tenji('PA N DA')
+    assert_equal <<~TENJI.chomp, tenji
+      -- o- -- -- o-
+      -- -- -o -o -o
+      -o oo oo -- o-
+    TENJI
+  end
+
+  # キャメル
+  def test_kya_me_ru
+    tenji = @tenji_maker.to_tenji('KYA ME RU')
+    assert_equal <<~TENJI.chomp, tenji
+      -o o- oo oo
+      -- -- oo -o
+      -- -o oo --
+    TENJI
+  end
+
+  # シャチ
+  def test_sya_ti
+    tenji = @tenji_maker.to_tenji('SYA TI')
+    assert_equal <<~TENJI.chomp, tenji
+      -o o- o-
+      -- -o oo
+      -- -o o-
+    TENJI
+  end
+
+  # ニャンちゅう（ニャンチュウ）
+  def test_nya_n_tyu
+    tenji = @tenji_maker.to_tenji('NYA N TYU')
+    assert_equal <<~TENJI.chomp, tenji
+      -o o- -- -o oo
+      -- -- -o -- -o
+      -- o- oo -- o-
+    TENJI
+  end
+
+  # クロヒョウ
+  def test_ku_ro_hyo
+    tenji = @tenji_maker.to_tenji('KU RO HYO')
+    assert_equal <<~TENJI.chomp, tenji
+      oo -o -o -o
+      -- oo -- o-
+      -o -- -- oo
+    TENJI
+  end
+
+  # エミュー
+  def test_e_myuu
+    tenji = @tenji_maker.to_tenji('E MYU -')
+    assert_equal <<~TENJI.chomp, tenji
+      oo -o oo --
+      o- -- -o oo
+      -- -- oo --
+    TENJI
+  end
+
+  # リャマ
+  def test_rya_ma
+    tenji = @tenji_maker.to_tenji('RYA MA')
+    assert_equal <<~TENJI.chomp, tenji
+      -o o- o-
+      -- -o -o
+      -- -- oo
+    TENJI
+  end
+
+  # キンギョ
+  def test_ki_n_gyo
+    tenji = @tenji_maker.to_tenji('KI N GYO')
+    assert_equal <<~TENJI.chomp, tenji
+      o- -- -o -o
+      o- -o -o o-
+      -o oo -- -o
+    TENJI
+  end
+
+  # クジャク
+  def test_ku_zya_ku
+    tenji = @tenji_maker.to_tenji('KU ZYA KU')
+    assert_equal <<~TENJI.chomp, tenji
+      oo -o o- oo
+      -- -o -o --
+      -o -- -o -o
+    TENJI
+  end
+
+  # クヂャク（クジャク）
+  def test_ku_dya_ku
+    tenji = @tenji_maker.to_tenji('KU DYA KU')
+    assert_equal <<~TENJI.chomp, tenji
+      oo -o o- oo
+      -- -o -o --
+      -o -- o- -o
+    TENJI
+  end
+
+  # ピョン吉（ピョンキチ）
+  def test_pyo_n_ki_ti
+    tenji = @tenji_maker.to_tenji('PYO N KI TI')
+    assert_equal <<~TENJI.chomp, tenji
+      -o -o -- o- o-
+      -- o- -o o- oo
+      -o oo oo -o o-
+    TENJI
+  end
+
+  # 白虎（ビャッコ）
+  def test_bya_kko
+    tenji = @tenji_maker.to_tenji('BYA KKO')
+    assert_equal <<~TENJI.chomp, tenji
+      -o o- -- -o
+      -o -- o- o-
+      -- oo -- -o
+    TENJI
+  end
+
+  # ウォンバット（ヲンバット）
+  def test_wo_n_ba_tto
+    tenji = @tenji_maker.to_tenji('WO N BA TTO')
+    assert_equal <<~TENJI.chomp, tenji
+      -- -- -- o- -- -o
+      -o -o -o -- o- oo
+      o- oo -- oo -- o-
+    TENJI
+  end
+
+  # 吉四六さん（キッチョムサン）
+  def test_ki_ttyo_mu_sa_n
+    tenji = @tenji_maker.to_tenji('KI TTYO MU SA N')
+    assert_equal <<~TENJI.chomp, tenji
+      o- -- -o -o oo o- --
+      o- o- -- oo -o -o -o
+      -o -- -- o- oo -o oo
+    TENJI
+  end
 end
